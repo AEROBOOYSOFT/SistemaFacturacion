7ecccb3f17c4c9b368b6edf7934f979266c7a4da
aerobooy <fabelydominguez@gmail.com>
2025-01-12 09:10:08 -0400

    agregando variable

--- SistemaFacturacion/CLASES/Cliente.cs
+++ SistemaFacturacion/CLASES/Cliente.cs
@@ -14,5 +14,7 @@ namespace SistemaFacturacion.Clases
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
+
+        public string gff { get; set; }
     }
 }
