a7f8a94174dad77dcf5960ee6820648a1ed2a6d3
aerobooy <fabelydominguez@gmail.com>
2025-01-13 03:27:27 -0400

    #

--- SistemaFacturacion/CLASES/Cliente.cs
+++ SistemaFacturacion/CLASES/Cliente.cs
@@ -8,11 +8,11 @@ namespace SistemaFacturacion.Clases
 {
     public class Cliente
     {
-        public int IdCliente { get; set; }
+       public int IdCliente { get; set; }
        public string Nombre { get; set; }
        public string Cedula { get; set; }
        public string Direccion { get; set; }
        public string Telefono { get; set; }
-    
+       public string Email { get; set; }
     }
 }
