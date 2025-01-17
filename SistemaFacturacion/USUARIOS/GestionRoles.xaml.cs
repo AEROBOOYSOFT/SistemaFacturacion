ad08e6ce820e97b927da4de783c4d376535ad954
aerobooy <fabelydominguez@gmail.com>
2025-01-16 23:12:28 -0400

    #

---
+++ SistemaFacturacion/USUARIOS/GestionRoles.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaFacturacion.USUARIOS
+{
+    /// <summary>
+    /// Lógica de interacción para GestionRoles.xaml
+    /// </summary>
+    public partial class GestionRoles : Window
+    {
+        public GestionRoles()
+        {
+            InitializeComponent();
+        }
+    }
+}
